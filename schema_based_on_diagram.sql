2ed4051ed6e0aba4dcee6e83fc39c52901d23dd7
echchafykyotmane <otmaneechchafyky5@gmail.com>
2023-07-19 15:28:05 +0100

    Create indexes for foreign keys

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -72,3 +72,13 @@ ADD CONSTRAINT fk_invoice_items_treatment_id
 FOREIGN KEY (treatment_id)
 REFERENCES treatments (id);
 
+-- Create indexes on foreign key columns
+CREATE INDEX idx_medical_history_treatments_medical_history_id ON medical_history_treatments (medical_history_id);
+CREATE INDEX idx_medical_history_treatments_treatment_id ON medical_history_treatments (treatment_id);
+
+CREATE INDEX idx_invoice_items_invoice_id ON invoice_items (invoice_id);
+CREATE INDEX idx_invoice_items_treatment_id ON invoice_items (treatment_id);
+
+CREATE INDEX idx_invoices_medical_history_id ON invoices (medical_history_id);
+
+CREATE INDEX idx_medical_histories_patient_id ON medical_histories (patient_id);
